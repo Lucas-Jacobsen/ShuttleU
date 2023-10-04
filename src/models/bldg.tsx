60b2bb3d6e2f8c15ae73b8254915525dcb6ea721
Lucas-Jacobsen <lukejacobsen33@gmail.com>
2023-10-03 23:27:48 -0700

    bldg model

---
+++ src/models/bldg.tsx
@@ -0,0 +1,13 @@
+export class Bldg {
+    name: string;
+    number: number;
+    lat: number; 
+    long: number
+ 
+ constructor(name: string, number: number, lat: number, long: number) {
+   this.name = name;
+   this.number = number; 
+   this.lat = lat; 
+   this.long = long; 
+}
+}
